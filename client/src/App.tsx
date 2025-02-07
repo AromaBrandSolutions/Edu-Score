dc28f4ac7b511e93e57e3fd533001040c48f6d04
Pratik Nichit <nichitpratik2004@gmail.com>
2025-02-07 08:43:25 +0530

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography sx={{ fontWeight: 800 }} textAlign={"center"} color="primary">
+        EduScore
+      </Typography>
+    </>
+  );
+}
+
+export default App;
