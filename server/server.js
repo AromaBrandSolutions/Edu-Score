eaf35ee53db8cb82983f7f14123486b083e303bf
Pratik Nichit <nichitpratik2004@gmail.com>
2025-02-13 19:24:05 +0530

    tailwind config done

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-const app = express();
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+mongoose
+  .connect(process.env.DEV_CONNECTION_URL)
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
