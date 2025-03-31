f231093bb15f40cec7d65483ce674ccd60855b55
PratikNichit <nichitpratik2004@gmail.com>
2025-03-31 18:56:42 +0530

    boilerplate code for new desgin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,5 @@
-import "./App.css";
-import HeroSection from "./Components/Head";
-import MissionComponent from "./Components/MissionComponent";
-import FooterComponent from "./Components/FooterComponent";
-import Navbar from "./Components/Navbar";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <Navbar /> {/* Use the Navbar component here */}
-        <HeroSection />
-        <MissionComponent />
-        <FooterComponent />
-      </div>
-    </ThemeProvider>
-  );
+  return <div>hello world</div>;
 }
 
 export default App;
