079d89d983c8746fd94346e578ebc0c377b199e5
Anjali-Bhagwate <bhagwateanjali2810@gmail.com>
2025-04-02 15:52:52 +0530

    Student Dashboard Components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,37 @@
-import Demo from "./Components/Demo";
+import React from "react";
+import WelcomeBanner from "./Components/WelcomeBanner";
+import TotalExamsCard from "./Components/TotalExamsCard";
+import OngoingExams from "./Components/OngoingExams";
+import UpcomingExams from "./Components/UpcomingExams";  
+import RecentResults from "./Components/RecentResults"; 
 
-function App() {
-  return <Demo />;
-}
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      {/* Welcome Banner */}
+      <WelcomeBanner />
+
+      {/* Total Exams Card Section */}
+      <div className="mt-6">
+        <TotalExamsCard />
+      </div>
+
+      {/* Ongoing Exams Section */}
+      <div className="mt-6">
+        <OngoingExams />
+      </div>
+
+      {/* Upcoming Exams Section */}
+      <div className="mt-6">
+        <UpcomingExams />
+      </div>
+
+      {/* Recent Results Section */}
+      <div className="mt-6">
+        <RecentResults />
+      </div>
+    </div>
+  );
+};
 
 export default App;
